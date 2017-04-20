01df25c971e7969fa39560a96807ae393c1cae75
Carlos Roberto Garcia Junior <carlos-r-g-junior@hotmail.com>
2017-04-19 22:37:21 -0300

    Subindo Imagem

--- Ocr and Melanoma/Filters/Ocr.cs
+++ Ocr and Melanoma/Filters/Ocr.cs
@@ -51,7 +51,7 @@ namespace Filters
             Erosion3x3 e = new Erosion3x3();
             var binary = e.Apply(imagem);
 
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 5; i++)
             {
                 binary = e.Apply(binary);
             }
@@ -65,7 +65,7 @@ namespace Filters
             Dilatation3x3 e = new Dilatation3x3();
             var binary = e.Apply(imagem);
 
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 8; i++)
             {
                 binary = e.Apply(binary);
             }
